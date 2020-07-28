796c70b40598bde67a54f58356da644e21aa41e5
yazzy4 <yazburrell@pursuit.org>
2020-07-28 14:51:26 -0400

    first commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Yaz Burrell on 7/28/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
