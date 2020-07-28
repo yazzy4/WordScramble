00e2717987cd8a5f42b2271b6ada44689e030df9
yazzy4 <yazburrell@pursuit.org>
2020-07-28 16:10:10 -0400

    added closure method to begin capturing submit text action

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordUrl){
                 
@@ -30,11 +32,13 @@ class ViewController: UITableViewController {
         startGame()
     }
     
+    
     func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         usedWords.count
@@ -44,6 +48,26 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "word", for: indexPath)
         cell.textLabel?.text = allWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer(){
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else
+            { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String){
+        
     }
     
 
